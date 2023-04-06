88564ea69320da498cc47f2d091faa099184305a
Erika Ladner <erikaladner5@gmail.com>
2023-04-06 14:53:03 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import {Container } from '@mui/material'
+import Searcher from "./componets/Searchbar";
 
 const App = () => {
+  const {userStater, userState} = useState("octocat")
+  const {inputUser, setInputUser} = useState("userState")
+  
+
   return(
-    <div>Soy un platzinauta</div>
+    <Container sx={{
+      background:"whitesmoke",
+      width:"80vh",
+      height:"500px",
+      borderRadius:"16px",
+      marginTop:"40px",
+      display:"flex",
+      flexDirection: "column",
+      alignItems: "center"
+    }}>
+    <Searcher inputUser={inputUser} setInputUser={setInputUser}/>
+    </Container>
   )
 };
 
